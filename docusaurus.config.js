c81754f97efd1799a0e87ca52ba46d64858d088f
carefree0910 <syameimaru.saki@gmail.com>
2021-10-19 17:34:55 +0800

    📝Removed `blog` stuffs

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -37,7 +37,6 @@ module.exports = {
           label: 'Docs',
           position: 'left',
         },
-        {to: 'blog', label: 'Blog', position: 'left'},
         {
           href: 'https://github.com/carefree0910/carefree-learn',
           position: 'right',
@@ -75,19 +74,6 @@ module.exports = {
             },
           ],
         },
-        {
-          title: 'More',
-          items: [
-            {
-              label: 'Blog',
-              to: 'blog',
-            },
-            {
-              label: 'GitHub',
-              href: 'https://github.com/carefree0910/carefree-learn',
-            },
-          ],
-        },
       ],
       copyright: `Copyright © ${new Date().getFullYear()} carefree-learn, carefree0910. Built with Docusaurus.`,
     },
@@ -104,12 +90,6 @@ module.exports = {
           showLastUpdateTime: true,
           remarkPlugins: [math],
           rehypePlugins: [katex],
-        },
-        blog: {
-          showReadingTime: true,
-          // Please change this to your repo.
-          // editUrl:
-          //   'https://github.com/facebook/docusaurus/edit/master/website/blog/',
         },
         theme: {
           customCss: require.resolve('./src/css/custom.css'),
